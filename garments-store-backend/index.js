7cea3080af105b5104d40c3a663ac014c69cb4da
Aniket Kumar Ghosh <ghoshaniketkumar7@gmail.com>
2021-03-13 12:04:08 +0530

    basic command to connect with mongodb

--- garments-store-backend/index.js
+++ garments-store-backend/index.js
@@ -1,6 +1,17 @@
+// declaration of express
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8000; // port no, in which our backend server will run
+
+// declaration of mongodb and create connection
+const mongoose = require('mongoose');
+// actual database link is stored into process.env.DATABASE valriable
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true, // setting it false, prevents connecting (docs)
+    useCreateIndex: true, // uses mongo default indexing process for every new entry (docs)
+    useUnifiedTopology: true // helps to maintain a stable connection (docs)
+}).then(() => console.log(`db connected successfully on port 27017`)).catch(err => handleError(err)); // docs ==> if db cinnects successfull, calls the then call back else catches the error and handle it accordingly.
+
 
 app.get("/", (req, res) => res.send("Hello World!!"));
 
