5fd56b60cf05864d844f9799693d1a8a088e1ea6
Aniket Kumar Ghosh <ghoshaniketkumar7@gmail.com>
2021-04-15 13:05:09 +0530

    random.js added

---
+++ garments-store-backend/random.js
@@ -0,0 +1,12 @@
+// import { format } from "morgan";
+
+const smallletters = "qwertyuiopasdfghjklzxcvbnm", specialSymbols = "~!@#$%^&*()_+`={}[]<>,.?/|";
+const numbers = "1234567890", capitalLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+exports.getString = (length, characters = (smallletters + capitalLetters + numbers)) => {
+    let result = "";
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
