ad980678a596c9360cac9076304a21da4c59740b
Aniket Kumar Ghosh <ghoshaniketkumar7@gmail.com>
2021-04-19 21:48:08 +0530

    next focus orderSchemma

--- garments-store-backend/Routes/user.js
+++ garments-store-backend/Routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { isSignedIn, isAuthenticated, isAdmin } = require('../Controllers/auth');
-const { getUserById, getUser, updateUser, deleteUser, deleteAllUser, getAllUser, uploadProfilePic, getProfilePic, deleteProfilePic, addFollowing } = require('../Controllers/user');
+const { getUserById, getUser, updateUser, deleteUser, deleteAllUser, getAllUser, uploadProfilePic, getProfilePic, deleteProfilePic, addFollowing, removeFollowing } = require('../Controllers/user');
 const router = express.Router();
 
 // http://expressjs.com/en/5x/api.html#app.param
+// middlewares
 router.param("userId", getUserById);
+
 router.get("/:userId", isSignedIn, isAuthenticated, getUser); // done
 router.put("/:userId", isSignedIn, isAuthenticated, updateUser); 
 router.delete("/:userId", isSignedIn, isAuthenticated, deleteUser);
@@ -13,6 +15,7 @@ router.post("/:userId/profilePic", isSignedIn, isAuthenticated, uploadProfilePic
 router.get("/:userId/profilePic", isSignedIn, isAuthenticated, getProfilePic);
 router.delete("/:userId/profilePic", isSignedIn, isAuthenticated, deleteProfilePic);
 router.post("/:userId/addFollowing", isSignedIn, isAuthenticated, addFollowing);
+router.post("/:userId/removeFollowing", isSignedIn, isAuthenticated, removeFollowing);
 
 //testing route
 router.get("/:userId/checkSignedIn", isSignedIn, isAuthenticated, (req, res) => res.status(200).json({
